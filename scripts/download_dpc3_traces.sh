e12422350999db4b27c296f7eb9a583e3c78c134
kyama-st <sakura.kiyama422@gmail.com>
2022-08-09 17:32:24 +0900

    modify download_trace.sh

--- scripts/download_dpc3_traces.sh
+++ scripts/download_dpc3_traces.sh
@@ -7,3 +7,5 @@ mkdir -p $PWD/../dpc3_traces
 # done < dpc3_max_simpoint.txt
 
 wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/400.perlbench-41B.champsimtrace.xz
+
+cd ../
